55b3d50682fc30b06e1dcc6b1220e6f5bbb72e09
Peter MacMillan <peter@writeopen.com>
2013-08-07 19:47:26 -0400

    show more stuff on the table

--- app.js
+++ app.js
@@ -2,20 +2,40 @@
 
 var app = angular.module('stockQuote', []);
 
+
 app.service('UserStocks', function() {
   return {
-    data: [],
+    data: [
+      { symbol: 'GME', qty: 100, price: 47.50, comm: 9.99 },
+      { symbol: 'MSFT', qty: 200, price: 31.25, comm: 9.99 }
+    ],
 
-    addSymbol: function(symbol) {
+    add: function(symbol) {
       this.data.push({
         symbol: symbol, qty: 0, price: 0, comm: 0
       });
+    },
+
+    remove: function(row) {
+      var index = this.data.indexOf(row);
+
+      if (~index) {
+        this.data.splice(index, 1);
+      }
     }
   };
 });
 
 app.controller('StockListController', ['$scope', 'UserStocks', function($scope, UserStocks) {
   $scope.data = UserStocks.data;
+
+  $scope.valueOf = function(row) {
+    return row.qty * row.price;
+  };
+
+  $scope.remove = function(row) {
+    UserStocks.remove(row);
+  };
 }]);
 
 
@@ -23,7 +43,7 @@ app.controller('AddSymbolFormController', ['$scope', 'UserStocks', function($sco
   $scope.stock = { symbol: '' };
 
   $scope.addStock = function() {
-    UserStocks.addSymbol($scope.stock.symbol);
+    UserStocks.add($scope.stock.symbol);
 
     $scope.stock.symbol = '';
   };
